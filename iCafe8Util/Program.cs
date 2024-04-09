8e177166860f87e11fe9aa8e07eda5912b74f95e
Author Name <toper.carpediem@gmail.com>
2024-04-09 21:11:42 +0800

    Initial commit for updating game disk drive and default group name for iCafe8

---
+++ iCafe8Util/Program.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Data.Sqlite;
+using Microsoft.Extensions.Configuration;
+
+var builder = new ConfigurationBuilder()
+                 .AddJsonFile($"appsettings.json", true, true);
+
+var configuration = builder.Build();
+
+try
+{
+    var barserverLocation = configuration.GetConnectionString("BarServer");
+
+    using var connection = new SqliteConnection($"Data Source={barserverLocation}");
+
+    connection.Open();
+
+    var gameDiskDrive = configuration["Defaults:GameDiskDrive"];
+
+    using var command = connection.CreateCommand();
+
+    command.CommandText = @"UPDATE tbl_IcafeServerDiskCfg SET ClientSymbol = $gameDiskDrive WHERE ServerSymbolPurpose = 1";
+
+    command.Parameters.AddWithValue("$gameDiskDrive", gameDiskDrive);
+
+    await command.ExecuteNonQueryAsync();
+
+    var groupName = configuration["Defaults:GroupName"];
+
+    command.CommandText = @"UPDATE tbl_GroupName SET Name = $groupName WHERE GroupId = 1";
+
+    command.Parameters.AddWithValue("$groupName", groupName);
+
+    await command.ExecuteNonQueryAsync();
+
+}
+catch (Exception)
+{
+    Console.WriteLine("It's likely that your bar server is running, or perhaps this app is not running in an elevated mode (run as admin)");
+    
+    Console.WriteLine("Please also check if your the file path of barserver is correct in your current setup");
+}
+
